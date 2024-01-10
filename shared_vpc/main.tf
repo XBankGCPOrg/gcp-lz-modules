8174380ed7cf8ee97ac4fae29989045196460a43
Rahul Kumar <rahkumar49@publicisgroupe.net>
2024-01-10 13:02:20 +0530

    Updating the varriable names

--- shared_vpc/main.tf
+++ shared_vpc/main.tf
@@ -1,10 +1,10 @@
 # Enable A Shared VPC in the host project
 resource "google_compute_shared_vpc_host_project" "host" {
-  project = var.project_vending.project # Replace this with your host project ID in quotes
+  project = var.shared_vpc.project # Replace this with your host project ID in quotes
 }
 
 # To attach a first service project with host project 
 resource "google_compute_shared_vpc_service_project" "service" {
   host_project    = google_compute_shared_vpc_host_project.host.project
-  service_project = var.project_vending.service_project # Replace this with your service project ID in quotes
+  service_project = var.shared_vpc.service_project # Replace this with your service project ID in quotes
 }
