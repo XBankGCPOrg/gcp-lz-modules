16792c0a51ad2097f66f6d1582b7db771da6d328
Rahul Kumar <rahkumar49@publicisgroupe.net>
2024-01-18 19:37:30 +0530

    Updated code for shared vpc

--- shared_vpc/main.tf
+++ shared_vpc/main.tf
@@ -1,22 +1,25 @@
 # Enable A Shared VPC in the host project
 resource "google_compute_shared_vpc_host_project" "host" {
-  project = var.shared_vpc.project # Replace this with your host project ID in quotes
+  project = var.shared_vpc.host_project
 }
 
 # To attach a first service project with host project 
 resource "google_compute_shared_vpc_service_project" "service" {
   host_project    = google_compute_shared_vpc_host_project.host.project
-  service_project = var.shared_vpc.service_project # Replace this with your service project ID in quotes
+  service_project = var.shared_vpc.service_project
 }
 
+# Create IAM bindings for each subnet
 resource "google_project_iam_member" "shared_subnet" {
-  project = var.shared_vpc.project
+  for_each = toset(var.shared_vpc.subnet)
+
+  project = var.shared_vpc.host_project
   role    = "roles/compute.networkUser"
   member  = var.shared_vpc.service_account
 
   condition {
     title       = "shared_subnets"
     description = "Shared Subnet"
-    expression  = "resource.name.startsWith('${var.shared_vpc.subnet}')"
+    expression  = "resource.name.startsWith('${each.key}')"
   }
 }
