c158760b33a2ae5137e8ec73e9eba5ba9566556f
Manish Shanker <manish.shanker@publicissapient.com>
2024-01-18 19:14:20 +0000

    v2

--- shared_vpc/main.tf
+++ shared_vpc/main.tf
@@ -8,24 +8,28 @@ terraform {
     }
   }
 }
-
+# Enable A Shared VPC in the host project
 resource "google_compute_shared_vpc_host_project" "host" {
-  project = var.shared_vpc.project #Host project ID
+  project = var.shared_vpc.host_project
 }
 
+# To attach a first service project with host project 
 resource "google_compute_shared_vpc_service_project" "service" {
   host_project    = google_compute_shared_vpc_host_project.host.project
-  service_project = var.shared_vpc.service_project #Service Project ID
+  service_project = var.shared_vpc.service_project
 }
 
+# Create IAM bindings for each subnet
 resource "google_project_iam_member" "shared_subnet" {
-  project = var.shared_vpc.project
+  for_each = toset(var.shared_vpc.subnet)
+
+  project = var.shared_vpc.host_project
   role    = "roles/compute.networkUser"
   member  = var.shared_vpc.service_account
 
   condition {
-    title       = "shared_subnet"
-    description = "Subet Shared"
-    expression  = "resource.name.startWith('${var.shared_vpc.subnet}')"
+    title       = "shared_subnets"
+    description = "Shared Subnet"
+    expression  = "resource.name.startsWith('${each.key}')"
   }
 }
